ad0bc42acb584421966e1fcff58a919ed38d8ff9
Wilson Lin <contact@wilsonl.in>
2018-02-18 22:12:10 +1300

    Don't install postfix with tlp

--- subscripts/tlp.sh
+++ subscripts/tlp.sh
@@ -8,8 +8,11 @@ sudo add-apt-repository -y ppa:linrunner/tlp
 sudo apt update
 
 # Don't need tlp-rdw (plus it causes problems)
-sudo apt install -y \
-    tlp
+# tlp installs postfix, so don't install recommendations
+sudo apt install --no-install-recommends -y \
+    tlp \
+    linux-tools-generic \
+    linux-tools-common
 
 # Configure TLP
 sudo sed -i 's/^RESTORE_DEVICE_STATE_ON_STARTUP=.*$/RESTORE_DEVICE_STATE_ON_STARTUP=1/' /etc/default/tlp
