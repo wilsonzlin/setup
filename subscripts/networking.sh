840ceb15163d661b681c3bc91077ba491806b759
Wilson Lin <contact@wilsonl.in>
2018-05-26 19:37:18 +1200

    Remove advanced networking tuning

--- subscripts/networking.sh
+++ subscripts/networking.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Tune Linux networking
-echo 'net.ipv4.ip_local_port_range = 1024 65535' | sudo tee -a /etc/sysctl.conf
-echo 'net.core.somaxconn = 4096' | sudo tee -a /etc/sysctl.conf
-
 # Enable firewall
 sudo ufw enable
 
