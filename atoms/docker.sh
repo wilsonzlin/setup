fdccce737821e611c7b27b33117954bea0e196b6
Wilson Lin <contact@wilsonl.in>
2018-07-10 23:29:29 +1200

    Docker

---
+++ atoms/docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $sl_lsb_release \
+   stable"
+
+sudo apt update
+
+sudo apt install docker-ce
