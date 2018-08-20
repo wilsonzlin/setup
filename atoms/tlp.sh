db7a9d65198bae2c59883e36a9460e7604e8af5c
Wilson Lin <contact@wilsonl.in>
2018-08-20 23:25:43 +1200

    Prevent Bluetooth suspending

--- atoms/tlp.sh
+++ atoms/tlp.sh
@@ -15,3 +15,5 @@ sudo apt install --no-install-recommends -y \
 
 # Configure TLP
 sudo sed -i 's/^RESTORE_DEVICE_STATE_ON_STARTUP=.*$/RESTORE_DEVICE_STATE_ON_STARTUP=1/' /etc/default/tlp
+# Don't suspend Bluetooth as this breaks Bluetooth support
+sudo sed -i 's/^USB_BLACKLIST_BTUSB=.*$/USB_BLACKLIST_BTUSB=1/' /etc/default/tlp
