0f16cdd6fed85642ebcb26ab864ed889510eb146
Wilson Lin <contact@wilsonl.in>
2018-10-12 23:29:53 +1300

    Fix pgAdmin

--- atoms/pgadmin.sh
+++ atoms/pgadmin.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ ${sl_lsb_release}-pgdg main" > sudo tee /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ ${sl_lsb_release}-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
