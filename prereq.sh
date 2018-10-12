d66141b350f8ac5f1b1613ac045dfce0c0a8a759
Wilson Lin <contact@wilsonl.in>
2018-10-12 23:30:16 +1300

    snapd

--- prereq.sh
+++ prereq.sh
@@ -4,6 +4,6 @@ set -e
 
 sudo apt update
 sudo apt dist-upgrade -y
-sudo apt install -y curl wget software-properties-common
+sudo apt install -y curl wget software-properties-common snapd
 
 exit 0
