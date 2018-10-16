9a6b317f1f018cc20272e894a82d1b2f16124272
Wilson Lin <contact@wilsonl.in>
2018-10-17 10:30:26 +1300

    Install jq and PostgreSQL tools

--- atoms/pgadmin.sh
+++ atoms/pgadmin.sh
@@ -6,4 +6,4 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ ${sl_lsb_release}-pgdg main" 
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
-sudo apt install -y pgadmin4
+sudo apt install -y pgadmin4 postgresql-client-10
