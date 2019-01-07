55d211b0e6a255f9d2be5e88419068c6eab1fe5a
Wilson Lin <contact@wilsonl.in>
2019-01-07 22:05:05 +1100

    Fix docker

--- atoms/docker.sh
+++ atoms/docker.sh
@@ -8,13 +8,12 @@ sudo apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $sl_lsb_release \
-   stable"
+# Don't use add-apt-repository, it depends on old python3-urllib3 which is likely overridden with never pip-installed version
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $sl_lsb_release stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt update
 
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 sudo usermod -a -G docker "$USER"
