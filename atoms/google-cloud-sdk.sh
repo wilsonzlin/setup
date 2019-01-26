d15b5066f7abdb338691a8e0c70b274c6275dd8a
Wilson Lin <contact@wilsonl.in>
2019-01-26 16:41:13 +1100

    GCP SDK

--- atoms/google-cloud-sdk.sh
+++ atoms/google-cloud-sdk.sh
@@ -8,5 +8,5 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo apt-get update
-sudo apt-get install -y google-cloud-sdk
+sudo apt update
+sudo apt install -y google-cloud-sdk
