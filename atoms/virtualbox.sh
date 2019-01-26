257a257238cb4ce12e6462e8947ca4116166fa6d
Wilson Lin <contact@wilsonl.in>
2019-01-26 12:18:56 +1100

    Update VirtualBox to 6

--- atoms/virtualbox.sh
+++ atoms/virtualbox.sh
@@ -10,7 +10,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 sudo apt update
 
 sudo apt install -y \
-    virtualbox-5.2 \
+    virtualbox-6.0 \
     dkms
 
 VBOX_VERSION=$(vboxmanage -v)
