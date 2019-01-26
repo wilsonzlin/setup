bff9c593b5b9f7733755b73ad4f70fee44d009aa
Wilson Lin <contact@wilsonl.in>
2019-01-26 16:41:07 +1100

    Minor changes

--- atoms/docker.sh
+++ atoms/docker.sh
@@ -8,7 +8,6 @@ sudo apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Don't use add-apt-repository, it depends on old python3-urllib3 which is likely overridden with never pip-installed version
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $sl_lsb_release stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list
 
--- atoms/zsh.sh
+++ atoms/zsh.sh
@@ -6,4 +6,6 @@ sudo apt install -y zsh
 
 chsh -s $(which zsh)
 
+printf '\033[0;31m\033[1m!!! oh-my-zsh installer will automatically launch zsh, exit it to continue setup !!!\n\033[0m'
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
