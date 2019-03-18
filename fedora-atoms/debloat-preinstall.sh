3e8f1f465fd198616ec68e6cb16677ec6ad57e54
Wilson Lin <contact@wilsonl.in>
2019-03-18 20:06:31 +1100

    Fix Fedora debloat script bad condition

--- fedora-atoms/debloat-preinstall.sh
+++ fedora-atoms/debloat-preinstall.sh
@@ -13,7 +13,7 @@ packages_to_remove_filtered=""
 
 for pkg in $(echo $packages_to_remove); do
     sudo dnf list installed "$pkg" &> /dev/null
-    if [ ! "$?" -eq 0 ]; then
+    if [ "$?" -eq 0 ]; then
         packages_to_remove_filtered="$packages_to_remove_filtered $pkg"
     fi
 done
