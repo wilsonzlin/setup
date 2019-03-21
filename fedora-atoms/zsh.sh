08630160531cbdd9d6779b7fef14f8bb10c43a43
Wilson Lin <contact@wilsonl.in>
2019-03-21 22:27:31 +1100

    Don't repeat chsh

--- fedora-atoms/zsh.sh
+++ fedora-atoms/zsh.sh
@@ -4,8 +4,7 @@ set -e
 
 sudo dnf install -y zsh
 
-chsh -s $(which zsh)
-
 printf '\033[0;31m\033[1m!!! oh-my-zsh installer will automatically launch zsh, exit it to continue setup !!!\n\033[0m'
 
+# oh-my-zsh will chsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
