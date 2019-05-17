efb18a5bba31f258bfc8898faecb93efb95fea44
Wilson Lin <contact@wilsonl.in>
2019-05-18 09:12:55 +1000

    Use python3-dev instead of specific version as different distros come with different versions

--- ubuntu-atoms/aws.sh
+++ ubuntu-atoms/aws.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt install -y python3.6-dev python3-pip python3-setuptools python3-wheel
+sudo apt install -y python3-dev python3-pip python3-setuptools python3-wheel
 
 # Repo version of awscli is out of date
 pip3 install --user awscli aws-shell
