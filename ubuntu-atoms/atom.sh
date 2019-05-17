94309c004714dc0fd0736ff3f31ff906caecf798
Wilson Lin <contact@wilsonl.in>
2019-05-18 08:52:51 +1000

    Atom

---
+++ ubuntu-atoms/atom.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt update
+
+sudo apt-get install atom
