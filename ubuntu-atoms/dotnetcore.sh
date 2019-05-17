7728e9b65289370d4a67bc444b1f1e1d8ebbfa2d
Wilson Lin <contact@wilsonl.in>
2019-05-18 08:52:44 +1000

    .NET Core SDK

---
+++ ubuntu-atoms/dotnetcore.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd "$( mktemp -d )"
+
+wget -q "$sl_dotnetcore_sdk_url" -O dotnetcore.deb
+
+sudo dpkg -i dotnetcore.deb
+
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y dotnet-sdk-2.2
+
+popd
