34f77c526cd1666bafdf7d35e88aaf0d39db52ae
Wilson Lin <contact@wilsonl.in>
2019-05-18 09:12:27 +1000

    Make Atom install non-interactive

--- ubuntu-atoms/atom.sh
+++ ubuntu-atoms/atom.sh
@@ -3,7 +3,6 @@
 set -e
 
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+echo 'deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main' | sudo tee /etc/apt/sources.list.d/atom.list
 sudo apt update
-
-sudo apt-get install atom
+sudo apt install -y atom
