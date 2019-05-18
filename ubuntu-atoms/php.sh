415fe93309e64745d143d34da26bd2f49b7899a1
Wilson Lin <contact@wilsonl.in>
2019-05-18 15:36:18 +1000

    PHP

---
+++ ubuntu-atoms/php.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo add-apt-repository -y ppa:ondrej/php
+
+sudo apt install -y php7.3
+
+# php7.3 installs apache2 automatically, so disable it; it's unnecessary and might conflict with other HTTP servers.
+sudo systemctl disable apache2
