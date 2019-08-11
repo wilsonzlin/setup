ca66bb855309c9251db7dafc61dc1c43732be122
Wilson Lin <contact@wilsonl.in>
2019-08-11 23:25:36 +1000

    Add pip user installs to PYTHONPATH

--- ubuntu-atoms/pip.sh
+++ ubuntu-atoms/pip.sh
@@ -2,3 +2,5 @@
 
 # Add pip user installs to PATH.
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.profile"
+# Add pip user installs to PYTHONPATH
+echo 'export PYTHONPATH="$HOME/.local:$PYTHONPATH"' >> "$HOME/.profile"
