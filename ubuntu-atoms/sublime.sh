eabd71c8a3f5627eba46b90bb78fe541fe4e934e
Wilson Lin <contact@wilsonl.in>
2019-09-03 21:47:31 +1000

    Sublime Text

---
+++ ubuntu-atoms/sublime.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install -y sublime-text
