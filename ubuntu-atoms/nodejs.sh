9397ba21ec0599d3b798d3b8ff8d7697ef41e694
Wilson Lin <contact@wilsonl.in>
2019-09-10 16:24:13 +1000

    NVM

--- ubuntu-atoms/nodejs.sh
+++ ubuntu-atoms/nodejs.sh
@@ -5,3 +5,6 @@ set -e
 curl -sL "https://deb.nodesource.com/setup_$sl_node_version.x" | sudo -E bash -
 
 sudo apt install -y build-essential nodejs
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
