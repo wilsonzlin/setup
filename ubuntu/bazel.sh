58189ce109ee8b7c0e62d65a52202b1ea93c3310
Wilson Lin <code@wilsonl.in>
2019-12-03 16:27:38 +1100

    Bazel and GNOME Web

---
+++ ubuntu/bazel.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo apt update
+sudo apt install -y bazel
