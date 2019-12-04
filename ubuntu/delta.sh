bc2b7c6e78dd7601b3c5e4369dd20c4133656791
Wilson Lin <code@wilsonl.in>
2019-12-04 22:34:04 +1100

    Remove existing delta

--- ubuntu/delta.sh
+++ ubuntu/delta.sh
@@ -6,6 +6,12 @@ pushd "$(mktemp -d)" > /dev/null
 
 wget "$sl_delta_url" -O delta.deb
 
+# Remove existing different package named `delta`.
+sudo apt purge -y delta
+
 sudo dpkg -i delta.deb
+
+# Prevent restoring different repository version.
+sudo apt-mark hold delta
 
 popd > /dev/null
