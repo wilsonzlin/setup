d9040a6ba3a4efa873162cd4bc38d5e154c6682c
Wilson Lin <code@wilsonl.in>
2020-03-01 16:14:28 +1100

    Fix rbenv dependencies installation

--- ubuntu/ruby.sh
+++ ubuntu/ruby.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+for p in autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm6 libgdbm-dev; do
+  # Some packages may not be available.
+  sudo apt install -y $p || true
+done
 
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
