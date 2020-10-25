d0dcf8690e74364521d4344b7cad9801425f80ba
Wilson Lin <code@wilsonl.in>
2020-10-25 22:26:31 +1100

    Add CUDA; fix Docker

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -6,6 +6,15 @@ sudo apt install -y docker.io
 
 sudo usermod -a -G docker "$USER"
 
-curl "$sl_docker_compose_url" -o "$HOME/Applications/docker-compose"
+# This URL will redirect, so make sure to follow redirects.
+wget "$sl_docker_compose_url" -O "$HOME/Applications/docker-compose"
 chmod +x "$HOME/Applications/docker-compose"
 ln -s "$HOME/Applications/docker-compose" "$HOME/bin/"
+
+if command -v nvidia-smi &> /dev/null; then
+  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+  sudo apt update
+  sudo apt install -y nvidia-docker2
+fi
