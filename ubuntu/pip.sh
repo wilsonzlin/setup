a00316e209bc3ef94ad66c019c19da7468fa4102
Wilson Lin <code@wilsonl.in>
2020-10-26 13:21:20 +1100

    pipenv

--- ubuntu/pip.sh
+++ ubuntu/pip.sh
@@ -4,3 +4,5 @@
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.profile"
 # Add pip user installs to PYTHONPATH
 echo 'export PYTHONPATH="$HOME/.local${PYTHONPATH:+:${PYTHONPATH}}"' >> "$HOME/.profile"
+
+pip3 install pipenv
