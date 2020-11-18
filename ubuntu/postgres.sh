589e40e0c14f9406307ce709d002d99cba25a237
Wilson Lin <code@wilsonl.in>
2020-11-19 10:31:17 +1100

    pgcli and pspg; use rustup for Windows instead of Chocolatey

--- ubuntu/postgres.sh
+++ ubuntu/postgres.sh
@@ -6,4 +6,12 @@ echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ ${sl_lsb_release
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
-sudo apt install -y postgresql
+sudo apt install -y postgresql libpq-dev
+
+pip3 install pgcli
+sudo apt install -y pspg
+mkdir -p "$HOME/.config/pgcli"
+cat << 'EOD' >> "$HOME/.config/pgcli/config"
+pager = /usr/bin/pspg --csv --rr=2 --quit-if-one-screen --ignore-case --csv-header on
+table_format = csv
+EOD
