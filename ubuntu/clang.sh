676f16a584fddcb85d71ca136f171fef012ff72a
Wilson Lin <code@wilsonl.in>
2021-05-24 22:05:16 +1000

    Fixes

--- ubuntu/clang.sh
+++ ubuntu/clang.sh
@@ -9,4 +9,8 @@ pushd "$(mktemp -d)" > /dev/null
 curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-$sl_lsb_release_version.tar.xz" | tar -Jxvf -
 mv clang+llvm* "$HOME/Applications/llvm"
 
+pushd "$HOME/Applications"
+ln -s "$HOME/Applications/llvm/bin" llvm
+popd
+
 popd > /dev/null
--- ubuntu/golang.sh
+++ ubuntu/golang.sh
@@ -6,8 +6,8 @@ pushd "$(mktemp -d)" > /dev/null
 
 # Avoid using repo managed by system, as they're not officially supported by Go and have caused dependency resolution issues in the past.
 
-wget 'https://golang.org/dl/go1.16.4.linux-amd64.tar.gz'
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+curl -L 'https://golang.org/dl/go1.16.4.linux-amd64.tar.gz' | sudo tar -C /usr/local -zxf -
 echo 'export PATH=$PATH:/usr/local/go/bin' >> "$HOME/.zshenv"
 
 popd > /dev/null
