5acd8d4fe9d26e8696d200543281940235f79ff5
Wilson Lin <code@wilsonl.in>
2021-05-24 21:41:38 +1000

    Fix arg

--- ubuntu/clang.sh
+++ ubuntu/clang.sh
@@ -6,6 +6,6 @@ pushd "$(mktemp -d)" > /dev/null
 
 # Avoid using apt.llvm.org as there can be conflicts with package resolution from previous experiences.
 
-curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-$sl_lsb_release_version.tar.xz" | tar -zxvf - -C "$HOME/Applications"
+curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-$sl_lsb_release_version.tar.xz" | tar -Jxvf - -C "$HOME/Applications"
 
 popd > /dev/null
