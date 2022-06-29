e83c97825d3f51dc5a9a8245abc6c4b072fcabbe
Wilson Lin <code@wilsonl.in>
2022-06-30 00:53:29 +1000

    Bump versions

--- ubuntu/golang.sh
+++ ubuntu/golang.sh
@@ -1,5 +1,5 @@
 # Avoid using repo managed by system, as they're not officially supported by Go and have caused dependency resolution issues in the past.
 
 sudo rm -rf /usr/local/go
-curl -L 'https://golang.org/dl/go1.16.4.linux-amd64.tar.gz' | sudo tar -C /usr/local -zxf -
+curl -L "$sl_golang_url" | sudo tar -C /usr/local -zxf -
 echo 'export PATH="$PATH:/usr/local/go/bin"' >> "$HOME/.zshenv"
--- ubuntu/nodejs.sh
+++ ubuntu/nodejs.sh
@@ -4,4 +4,4 @@ curl -sL "https://deb.nodesource.com/setup_18.x" | sudo -E bash -
 sudo apt install -y nodejs
 
 # Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- "$sl_nvm_url" | bash
